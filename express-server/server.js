d0f072cacd1e6cf1f1f8b3c0521f08eecc3afa01
Steve Phelps <79sphelps@gmail.com>
2019-03-15 07:57:52 -0700

    adding concurrently package to dev dependencies

--- express-server/server.js
+++ express-server/server.js
@@ -3,13 +3,8 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import mongoose from 'mongoose';
 import SourceMapSupport from 'source-map-support';
 import bb from 'express-busboy';
-
-
-
-
 
 // import routes
 import zillowRoutes from './routes/zillow.server.route';
@@ -21,12 +16,11 @@ const app = express();
 bb.extend(app);
 
 // allow-cors
-app.use(function(req,res,next){
+app.use(function(req, res,next){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 })
-
 
 // configure app
 app.use(logger('dev'));
@@ -34,20 +28,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // set the port
 const port = process.env.PORT || 3001;
-/*
-// connect to database
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/mern-todo-app', {
-  useMongoClient: true,
-});
-*/
 
 // add Source Map Support
 SourceMapSupport.install();
-
 
 app.use('/api', zillowRoutes);
 
@@ -57,71 +42,10 @@ app.get('/', (req,res) => {
 })
 */
 
-
-/*
-unirest.post("https://ZillowdimashirokovV1.p.rapidapi.com/getChart")
-.header("X-RapidAPI-Key", "5fa5d944a9msh9bf3a2a83a515d1p16d337jsna797538fb371")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
-
-console.log('---------------------------------------------------------');
-
-unirest.post("https://ZillowdimashirokovV1.p.rapidapi.com/getComps")
-.header("X-RapidAPI-Key", "5fa5d944a9msh9bf3a2a83a515d1p16d337jsna797538fb371")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.send("count=1")
-.send("zwsId=X1-ZWz1gy3v1sml8r_2f38x")
-.send("zpid=2000")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
-
-console.log('---------------------------------------------------------');
-
-unirest.post("https://ZillowdimashirokovV1.p.rapidapi.com/getDeepComps")
-.header("X-RapidAPI-Key", "5fa5d944a9msh9bf3a2a83a515d1p16d337jsna797538fb371")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
-
-console.log('---------------------------------------------------------');
-
-unirest.post("https://ZillowdimashirokovV1.p.rapidapi.com/getDeepSearchResults")
-.header("X-RapidAPI-Key", "5fa5d944a9msh9bf3a2a83a515d1p16d337jsna797538fb371")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
-
-
-console.log('---------------------------------------------------------');
-
-unirest.post("https://ZillowdimashirokovV1.p.rapidapi.com/getRegionChildren")
-.header("X-RapidAPI-Key", "5fa5d944a9msh9bf3a2a83a515d1p16d337jsna797538fb371")
-.header("Content-Type", "application/x-www-form-urlencoded")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
-
-
-console.log('---------------------------------------------------------');
-
-
-
-
-console.log('---------------------------------------------------------');
-*/
-
-
-
 // catch 404
 app.use((req, res, next) => {
   res.status(404).send('<h2 align=center>Page Not Found!</h2>');
 });
-
 
 // start the server
 app.listen(port,() => {
